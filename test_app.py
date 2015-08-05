75dc62ead0ca3fac7d25673cd86d749e44d5f599
Guilherme Louro <guilherme-louro@hotmail.com>
2015-08-05 17:54:16 -0300

    tests callback

--- test_app.py
+++ test_app.py
@@ -13,6 +13,24 @@ class FlaskAppTest(unittest.TestCase):
         self.index = self.client.get('/')
         self.assertEqual(self.index.status_code, 200)
 
+    def test_get_callback(self):
+        self.callback = self.client.get(
+            '/auth_callback',
+            data=dict(code='5bcee29da146435882b9d4fbb988bd98')
+        )
+        self.assertEqual(self.callback.status_code, 302)
+
+    def test_get_callback_bad_request(self):
+        self.callback = self.client.get('/auth_callback')
+        self.assertEqual(self.callback.status_code, 400)
+
+    def test_get_callback_missing(self):
+        self.callback = self.client.get(
+            '/auth_callback',
+            data=dict(code='')
+        )
+        self.assertEqual(self.callback.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
